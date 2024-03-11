c4d76e74271e2dc4410f0cbb794c4cf3d30a8001
kashiwade <music5d7_9265@outlook.jp>
2024-03-11 23:42:23 +0900

    add scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,143 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import { useRef } from "react";
+import { PageProps, HeadFC } from "gatsby";
+import * as vanilla from "../styles/index.css";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import { Observer } from "gsap/Observer";
+import { log } from "console";
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+gsap.registerPlugin(useGSAP, Observer);
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
+const SectionTemplate = React.forwardRef<
+  HTMLDivElement,
+  { children: React.ReactNode; sectionName: string }
+>(({ children, sectionName }, ref) => {
+  let className = "";
+  if (sectionName === "top") {
+    className = vanilla.SectionStyleTop;
+  } else if (sectionName === "profile") {
+    className = vanilla.SectionStyleProfile;
+  } else if (sectionName === "works") {
+    className = vanilla.SectionStyleWorks;
+  } else if (sectionName === "special") {
+    className = vanilla.SectionStyleSpecial;
+  } else if (sectionName === "links") {
+    className = vanilla.SectionStyleLinks;
+  } else if (sectionName === "contact") {
+    className = vanilla.SectionStyleContact;
   }
-]
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+  return (
+    <section className={className} ref={ref}>
+      <div className="outer">
+        <div className="inner">
+          <div className="bg">{children}</div>
+        </div>
+      </div>
+    </section>
+  );
+});
 
 const IndexPage: React.FC<PageProps> = () => {
+  const sectionNames = [
+    "top",
+    "profile",
+    "works",
+    "special",
+    "links",
+    "contact",
+  ];
+  let sectionRefs = useRef<HTMLDivElement[]>([]);
+
+  useGSAP(() => {
+    let animating = false;
+    let currentIndex = 0;
+
+    sectionRefs.current.forEach((sectionRef, index) => {
+      if (index !== 0) {
+        gsap.set(sectionRef, { autoAlpha: 0, yPercent: 100 });
+      }
+    });
+
+    const gotoSection = (index: number, direction: number) => {
+      if (
+        (index < 0 && direction === -1) ||
+        (index >= sectionRefs.current.length && direction === 1)
+      ) {
+        return;
+      }
+      console.log(
+        `Running. currentIndex=${currentIndex}, from: ${index} to: ${direction})`
+      );
+      console.log(`Wrapped index: ${index}`);
+
+      animating = true;
+
+      let tl = gsap.timeline({
+        defaults: { duration: 1.25, ease: "power2.inOut" },
+        onComplete: () => {
+          animating = false;
+        },
+      });
+
+      if (direction === 1) {
+        tl.to(sectionRefs.current[currentIndex], {
+          autoAlpha: 0,
+          yPercent: -100,
+          zIndex: 0,
+        });
+        tl.to(
+          sectionRefs.current[index],
+          { autoAlpha: 1, yPercent: 0, zIndex: 1 },
+          "<"
+        );
+      } else {
+        tl.to(sectionRefs.current[currentIndex], {
+          autoAlpha: 0,
+          yPercent: 100,
+          zIndex: 0,
+        });
+        tl.to(
+          sectionRefs.current[index],
+          { autoAlpha: 1, yPercent: 0, zIndex: 1 },
+          "<"
+        );
+      }
+
+      currentIndex = index;
+    };
+
+    Observer.create({
+      type: "wheel,touch,pointer",
+      wheelSpeed: -1,
+      onDown: () => !animating && gotoSection(currentIndex - 1, -1),
+      onUp: () => !animating && gotoSection(currentIndex + 1, 1),
+      tolerance: 10,
+      preventDefault: true,
+    });
+  }, [sectionRefs]);
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <main>
+      {sectionNames.map((sectionName, index) => {
+        return (
+          <SectionTemplate
+            key={sectionName}
+            sectionName={sectionName}
+            ref={(el) => {
+              sectionRefs.current[index] = el as HTMLDivElement;
+            }}
+          >
+            {sectionName}
+          </SectionTemplate>
+        );
+      })}
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
---
+++ src/styles/index.css.ts
@@ -0,0 +1,73 @@
+import { style } from "@vanilla-extract/css";
+
+export const SectionStyleTop = style({
+  height: "100%",
+  width: "100%",
+  top: "0",
+  position: "fixed",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "blue",
+  //visibility: "hidden",
+});
+
+export const SectionStyleProfile = style({
+  height: "100%",
+  width: "100%",
+  top: "0",
+  position: "fixed",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "green",
+  visibility: "hidden",
+});
+
+export const SectionStyleWorks = style({
+  height: "100%",
+  width: "100%",
+  top: "0",
+  position: "fixed",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "purple",
+  visibility: "hidden",
+});
+
+export const SectionStyleSpecial = style({
+  height: "100%",
+  width: "100%",
+  top: "0",
+  position: "fixed",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "orange",
+  visibility: "hidden",
+});
+
+export const SectionStyleLinks = style({
+  height: "100%",
+  width: "100%",
+  top: "0",
+  position: "fixed",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "yellow",
+  visibility: "hidden",
+});
+
+export const SectionStyleContact = style({
+  height: "100%",
+  width: "100%",
+  top: "0",
+  position: "fixed",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "skyblue",
+  visibility: "hidden",
+});
